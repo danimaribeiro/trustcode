fb65622988a852829a93fb174e9895243b442138
tiagaoprates <tiagoprates_911@hotmail.com>
2018-03-23 19:59:09 -0300

    2 - Atualizar cadastro do cliente recém criado

--- teste_desenvolvedor.py
+++ teste_desenvolvedor.py
@@ -14,8 +14,15 @@ class ConexaoOdoo():
         self.conn, self.uid = self.new_connection()
         cliente_id = self.create_cliente()
         print 'ID do cliente criado: %s.\n' % cliente_id
+        cliente_atualizado = self.update_cliente(cliente_id)
+        print 'Cliente atualizado: %s' % 'SIM' if cliente_atualizado else 'Nao'
 
     def new_connection(self):
+        """
+        O metodo estabalece a conexao com o Odoo.
+        :return: objeto da conexao e id do usuario logado.
+        :rtype: object
+        """
         if not self.password:
             self.password = None
         if not self.port:
@@ -33,11 +40,29 @@ class ConexaoOdoo():
             exec exit()
 
     def create_cliente(self):
+        """
+        O metodo cria um novo cliente.
+        :return: id da tabela res_partner
+        :rtype: int
+        """
         vals = {'name': 'Tiago Henrique Prates', 'email': 'tiagoprates_911@hotmail.com',
                 'phone': '98233-7159', 'zip': '35700-000'}
         cliente_id = self.conn.execute(self.database, self.uid, self.password,
                                        'res.partner', 'create', vals)
         return cliente_id
+
+    def update_cliente(self, cliente_id):
+        """
+        O metodo atualiza os dados de um registro.
+        :param cliente_id: id da tabela res_partner, identificador do cliente.
+        :type cliente_id: int
+        :return: Verdadeiro, caso consiga realizar a atualizacao. Senao, Falso.
+        :rtype: bool
+        """
+        vals = {'phone': '982648882'}  # Nao encontrei o campo RG, incluido CPF
+        cliante_atualizado = self.conn.execute(self.database, self.uid, self.password,
+                                               'res.partner', 'write', [cliente_id], vals)
+        return cliante_atualizado
 
 
 ConexaoOdoo()
